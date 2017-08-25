0550ec406a69057e503d243ebf8327286a19128e
Minje Lee <minjae8989@gmail.com>
2017-08-24 21:24:13 -0700

    api fully connected. ready to implement!

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,15 +9,28 @@ function receiveMemes(json) {
   }
 }
 
-function fetchMemesJson() {
-  return fetch('https://api.imgflip.com/get_memes')
-    .then(response => response.json())
-}
-
 //fetching nmemes asynchronously
 export function fetchMemes() {
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
   return function(dispatch) {
-    return fetchMemesJson()
-      .then(json => dispatch(receiveMemes(json)))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch('http://api.imgflip.com/get_memes')
+      .then(
+        response => response.json(),
+        error => console.log('An error occured', error)
+        )
+      .then(json =>
+        // We can dispatch many times!
+        // Here, we update the app state with the results of the API call
+        dispatch(receiveMemes(json)))
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App'
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
+import { fetchMemes } from './actions';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+store.subscribe(() => console.log('store', store.getState()))
+store.dispatch(fetchMemes());
+
 ReactDom.render(
-  <App />, document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
 )
